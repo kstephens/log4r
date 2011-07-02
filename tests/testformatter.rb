e3d8fa142c869dc7df803e27b5cad14e5ae651f7
Kurt Stephens <ks.github@kurtstephens.com>
2011-07-01 20:12:22 -0500

    Attempted to revive tests to support NullFormatter.

--- tests/testformatter.rb
+++ tests/testformatter.rb
@@ -1,4 +1,9 @@
-class TestFormatter < TestCase
+require 'test/unit'
+require 'log4r'
+
+module Log4r
+
+class TestFormatter < Test::Unit::TestCase
   def test_creation
     assert_no_exception { Formatter.new.format(3) }
     assert_no_exception { DefaultFormatter.new }
@@ -24,4 +29,21 @@ class TestFormatter < TestCase
     assert_match(b.format(event3), /ArgumentError/)
     assert_match(b.format(LogEvent.new(0,f,nil,[1,2,3])), /Array/)
   end
+  def test_non_string_data_events
+    require 'pp'
+    [ SimpleFormatter, BasicFormatter, NullFormatter ].each do | f_cls |
+      b = f_cls.new
+      f = Logger.new('fake formatter')
+      [ nil, 1, 1.234, "A String", :symbol, { :hash_a => 1, :hash_b => 2}, [ :array0, 1 ], TypeError.new("a type error") ].each do | data |
+        data.set_backtrace ['backtrace'] if data.respond_to?(:set_backtrace)
+        event = LogEvent.new(0, f, caller, data)
+        event2 = LogEvent.new(0, f, nil, data)
+        pp [ :event, f_cls, data, b.format(event) ]
+        pp [ :event2, f_cls, data, b.format(event2) ]
+      end
+    end
+  end
+
 end 
+
+end
